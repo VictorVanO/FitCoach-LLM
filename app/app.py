28a82bbdc1f0b5bbb402d9dfd0f685599dec97d3
VictorVanO <20253@ecam.be>
2025-11-18 13:59:13 +0100

    Added app.py file

---
+++ app/app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import numpy as np
+
+
+st.title("Fit Coach LLM")
+
+user_age = st.text_input("Enter your age:")
+user_gender = st.text_input("Enter your gender:")
+user_height = st.text_input("Enter your height:")
+user_weight = st.text_input("Enter your weight:")
+user_daily_time = st.text_input("Enter your time per day:")
+
+st.button("Send")
